3ec84920de99e32f89f523ebaf25d69dec5deb9e
Bob Dougherty <bob@mindstronghealth.com>
2018-11-05 17:21:44 -0800

    Soft-link libs aliases, don't build sklearn

--- build.sh
+++ build.sh
@@ -16,32 +16,32 @@ yum install -y \
 
 do_pip () {
     pip install --upgrade pip wheel
-    test -f /outputs/requirements.txt && pip install -r /outputs/requirements.txt
     pip install --no-binary numpy numpy
     pip install --no-binary scipy scipy
-    pip install sklearn
+    #pip install sklearn
+    test -f /outputs/requirements.txt && pip install -r /outputs/requirements.txt
 }
 
 strip_virtualenv () {
     echo "venv original size $(du -sh $VIRTUAL_ENV | cut -f1)"
-    find $VIRTUAL_ENV/lib64/python23.6/site-packages/ -name "*.so" | xargs strip
+    find $VIRTUAL_ENV/lib64/python3.6/site-packages/ -name "*.so" | xargs strip
     echo "venv stripped size $(du -sh $VIRTUAL_ENV | cut -f1)"
 
-    pushd $VIRTUAL_ENV/lib/python3.6/site-packages/ && zip -r -9 -q /tmp/partial-venv.zip * ; popd
-    pushd $VIRTUAL_ENV/lib64/python3.6/site-packages/ && zip -r -9 --out /outputs/venv.zip -q /tmp/partial-venv.zip * ; popd
+    pushd $VIRTUAL_ENV/lib/python3.6/site-packages/ && zip --symlinks -r -9 -q /tmp/partial-venv.zip * ; popd
+    pushd $VIRTUAL_ENV/lib64/python3.6/site-packages/ && zip --symlinks -r -9 --out /outputs/venv.zip -q /tmp/partial-venv.zip * ; popd
     echo "site-packages compressed size $(du -sh /outputs/venv.zip | cut -f1)"
 
-    pushd $VIRTUAL_ENV && zip -r -q /outputs/full-venv.zip * ; popd
+    pushd $VIRTUAL_ENV && zip --symlinks -r -q /outputs/full-venv.zip * ; popd
     echo "venv compressed size $(du -sh /outputs/full-venv.zip | cut -f1)"
 }
 
 shared_libs () {
     libdir="$VIRTUAL_ENV/lib64/python3.6/site-packages/lib/"
     mkdir -p $VIRTUAL_ENV/lib64/python3.6/site-packages/lib || true
-    cp /usr/lib64/atlas/* $libdir
-    cp /usr/lib64/libopenblas.so.0 $libdir
-    cp /usr/lib64/libquadmath.so.0 $libdir
-    cp /usr/lib64/libgfortran.so.3 $libdir
+    cp --preserve=links /usr/lib64/atlas/*.so* $libdir
+    cp --preserve=links /usr/lib64/libopenblas.so.0 $libdir
+    cp --preserve=links /usr/lib64/libquadmath.so.0 $libdir
+    cp --preserve=links /usr/lib64/libgfortran.so.3 $libdir
 }
 
 main () {
